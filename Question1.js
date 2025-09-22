b85534eaf01687aad4e483e7adca4f39d26fc263
Prince <anakwaprince204@gmail.com>
2025-09-21 20:13:09 -0700

    Code

--- Question1.js
+++ Question1.js
@@ -1,7 +1,21 @@
-function capitalizeWords(word){
-    
-upperWord= word.toUpperCase();
-    console.log(upperWord);
-}
 
-capitalizeWords("hello world")
+const capitalizeFirstLetter = (string) => {
+  
+  let words = string.split(" ");
+
+  
+  let capitalizedWords = words.map(word => {
+    let firstLetter = word.charAt(0).toUpperCase(); 
+    let restOfWord = word.slice(1); 
+    return firstLetter + restOfWord; 
+  });
+
+  
+  let result = capitalizedWords.join(" ");
+  
+ 
+  console.log(result);
+};
+
+capitalizeFirstLetter("hello world"); 
+
